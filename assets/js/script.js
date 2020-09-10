601553265a8c18a2952c8f8e4edbec1c357edf1c
Maksym Garus <bboykermo@gmail.com>
2020-09-10 15:40:35 +0200

    Corrections to navigations and overall fixes including better html representation and code refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
+let navigation = document.getElementById("navigation");
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset >= 300) {
+        navigation.classList.add("active", "navigation-white")
+    } else if (window.pageYOffset < 250) {
+        navigation.classList.remove("active", "navigation-white")
+    }
+})
+
 let openNavigation = $('.navbar-toggler')
 openNavigation.click(function () {
-  $('body').toggleClass("overflow-hidden")
-  $('#overlay').toggleClass("overlay-display")
+    $('body').toggleClass("overflow-hidden")
+    $('#whiteNavigationNavbar').toggleClass("nav-bg-light")
+    if ($(navigation).hasClass("active")) {
+        $('#navbar').toggleClass("nav-bg-light")
+    } else
+        $('#navbar').toggleClass("nav-bg-dark")
+})
+
+let darkNavigation = document.getElementById("whiteNavigation");
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset >= 300) {
+        darkNavigation.classList.add("active")
+    } else if (window.pageYOffset < 250) {
+        darkNavigation.classList.remove("active")
+    }
 })
